985d0661073994b054532fcd4d774b121918bc3f
bwatsonllvm <199831971+bwatsonllvm@users.noreply.github.com>
2026-04-12 09:38:58 +0000

    Library update sync (devmtg, blogs, papers)

--- js/shared/global-search.js
+++ js/shared/global-search.js
@@ -61,7 +61,7 @@
   }
 
   const LIBRARY_ROOT_PATH = resolveLibraryRootPath();
-  const AUTOCOMPLETE_INDEX_SRC = resolveAssetUrl('js/data/autocomplete-index.json?v=d3cf4347f18c');
+  const AUTOCOMPLETE_INDEX_SRC = resolveAssetUrl('js/data/autocomplete-index.json?v=1946b8f53e65');
   const ADVANCED_FIELDS = [
     'allWords',
     'exactPhrase',
