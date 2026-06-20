192798199a69782281241394ffbb455458a60f46
bwatsonllvm <199831971+bwatsonllvm@users.noreply.github.com>
2026-06-20 07:28:00 +0000

    Library papers sync (blogs, papers)

--- js/shared/global-search.js
+++ js/shared/global-search.js
@@ -61,7 +61,7 @@
   }
 
   const LIBRARY_ROOT_PATH = resolveLibraryRootPath();
-  const AUTOCOMPLETE_INDEX_SRC = resolveAssetUrl('js/data/autocomplete-index.json?v=3dd095dcf223');
+  const AUTOCOMPLETE_INDEX_SRC = resolveAssetUrl('js/data/autocomplete-index.json?v=1b409f6393fe');
   const ADVANCED_FIELDS = [
     'allWords',
     'exactPhrase',
